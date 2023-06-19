dde08b500e2920016669582a393a3210b7df863a
Nguyen Lam <lam.nguyen@icetea.io>
2023-06-19 16:02:54 +0700

    chore: add page thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,39 @@ import { inter } from '@/assets/fonts';
 import './globals.css';
 import { Metadata } from 'next';
 
+const defaultMetadata = {
+  title: 'BreakFirst - Embrace limits breaking. Make decentralization shines.',
+  description:
+    'An early stage web3 fund and incubator run by a team of seasoned crypto founders, incubators, and investors',
+  image: 'https://d1j2c9jkfhu70p.cloudfront.net/6fa4a73d-9997-4c97-af98-c1fb06b4d01d.png',
+  url: '',
+};
+
 export const metadata: Metadata = {
-  title: 'BreakFirst',
-  description: 'An early-stage web3 fund and incubator.',
+  title: defaultMetadata.title,
+  description: defaultMetadata.description,
+  openGraph: {
+    title: defaultMetadata.title,
+    description: defaultMetadata.description,
+    siteName: defaultMetadata.url,
+    images: [
+      {
+        url: defaultMetadata.image,
+        width: 1920,
+        height: 1080,
+        alt: 'BreakFirst thumbnail',
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: defaultMetadata.title,
+    description: defaultMetadata.description,
+    images: [defaultMetadata.image],
+  },
+  metadataBase: new URL(''),
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
